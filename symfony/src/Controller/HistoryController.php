9821485fef02fd4eff541b635de60e6bccbb604e
Solene <solene.bord@etudiant.univ-reims.fr>
2024-02-20 12:02:18 +0100

    feature: history page and pdf titles

--- symfony/src/Controller/HistoryController.php
+++ symfony/src/Controller/HistoryController.php
@@ -5,14 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Pdf;
+use App\Entity\User;
+use App\Repository\PdfRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HistoryController extends AbstractController
 {
-    #[Route('/history', name: 'app_history')]
-    public function index(): Response
+    // #[Route('/history', name: 'app_history')]
+    public function index(PdfRepository $pdfRepository, Security $security, EntityManagerInterface $entityManager): Response
     {
+
+        $user = $security->getUser();
+
+        $userId = $user->getId();
+
+
+        $pdfs = $entityManager->getRepository(Pdf::class)
+        ->findBy(['userId' => $userId]);
+
         return $this->render('history/index.html.twig', [
             'controller_name' => 'HistoryController',
+            'pdfs' => $pdfs,
         ]);
     }
 }
