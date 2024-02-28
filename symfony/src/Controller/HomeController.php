bb0754a8074b490a4b4e6e4217216182956283fb
Solene <solene.bord@etudiant.univ-reims.fr>
2024-02-28 11:23:41 +0100

    feature: homepage content

--- symfony/src/Controller/HomeController.php
+++ symfony/src/Controller/HomeController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Subscription;
+use App\Repository\SubscriptionRepository;
+
+
+
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    // #[Route('/home', name: 'app_home')]
+    public function index(SubscriptionRepository $subscriptionRepository): Response
     {
+        $subscriptions = $subscriptionRepository->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'subscriptions' => $subscriptions,
         ]);
     }
 }
